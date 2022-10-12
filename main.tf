2a0caabf3d11de2b4a423f4187f38b15d6d6c15f
Swapnil Hedau <swapnilhedau@hotmail.com>
2022-10-12 10:53:23 +0530

    linux VM. create ssh key using ssh-keygen -t rsa.

--- main.tf
+++ main.tf
@@ -93,3 +93,36 @@ resource "azurerm_network_interface" "mtc-nic" {
     environment = "dev"
   }
 }
+
+resource "azurerm_linux_virtual_machine" "mtc-vm" {
+  name                = "mtc-vm"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.mtc-nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("/Users/swapnilhedau/.ssh/mtcazurekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
