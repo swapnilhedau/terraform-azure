30a11527c1482b9543de5a01c1e7003206f1ac93
Swapnil Hedau <swapnilhedau@hotmail.com>
2022-10-12 10:09:29 +0530

    network interface

--- main.tf
+++ main.tf
@@ -76,3 +76,20 @@ resource "azurerm_public_ip" "mtc-ip" {
     environment = "dev"
   }
 }
+
+resource "azurerm_network_interface" "mtc-nic" {
+  name                = "mtc-nic"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mtc-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.mtc-ip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
