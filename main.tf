a7c473743e5cdbb37f65f71fd57851d7fbdbe80d
Swapnil Hedau <swapnilhedau@hotmail.com>
2022-10-17 10:47:32 +0530

    data source

--- main.tf
+++ main.tf
@@ -138,3 +138,8 @@ resource "azurerm_linux_virtual_machine" "mtc-vm" {
   }
 }
 
+data "azurerm_public_ip" "mtc-ip-data" {
+  name                = azurerm_public_ip.mtc-ip.name
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+}
+
